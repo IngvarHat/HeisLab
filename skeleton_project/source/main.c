db28e4a78f9bd6c07121b3671667a5543b7d00ac
Ingvar <ingvaha@stud.ntnu.no>
2025-02-26 19:15:15 +0100

    Fixed stop-button while finding floor

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -5,6 +5,9 @@
 #include "driver/elevio.h"
 #include "stdbool.h"
 
+
+
+
 typedef struct {
     int floor; 
     ButtonType button;
@@ -41,6 +44,7 @@ void printOrders() {
     for (int i = 0; i < orderCount; i++){
         printf("Order %d: Floor %d, Button %d\n", i, orderList[i].floor, orderList[i].button);
     }
+
 }
 
 void updateButtonLamp() {
@@ -58,28 +62,13 @@ void updateButtonLamp() {
     }
 }
 
+
+
 void StopButton() {
     if (elevio_stopButton()){
         elevio_motorDirection(DIRN_STOP);
         exit(0);
     }
-}
-
-int findNextOrder(int currentFloor, MotorDirection direction) {
-    if (direction == DIRN_UP) {
-        for (int i = 0; i < orderCount; i++) {
-            if (orderList[i].floor > currentFloor) {
-                return orderList[i].floor;
-            }
-        }
-    } else if (direction == DIRN_DOWN) {
-        for (int i = 0; i < orderCount; i++) {
-            if (orderList[i].floor < currentFloor) {
-                return orderList[i].floor;
-            }
-        }
-    }
-    return -1; // No orders in the current direction
 }
 
 int main(){
@@ -91,7 +80,8 @@ int main(){
 
     elevio_motorDirection(DIRN_DOWN);
 
-    // Oppstart - Flytter til etg 1 (0) bestillinger tas imot 
+
+    //Oppstart - Flytter til etg 1 (0) bestillinger tas imot 
     while(kalibrering == false){
         updateButtonLamp();
         floor = elevio_floorSensor();
@@ -101,27 +91,43 @@ int main(){
             kalibrering = true;
         }
 
+        if(kalibrering == true){
+        }
+
         StopButton();
         if(floor >= 0 && floor < 4){
             elevio_floorIndicator(floor);
         }
+
+
     }
     
-    MotorDirection direction = DIRN_UP;
-
+  
     while(kalibrering == true){
+        //printf("%d", kalibrering);
         floor = elevio_floorSensor();
+        // Node *bestillingsliste =NULL; //Setter bestillingsliste 0
+
+        //while(bestillingsliste == NULL){
+        //    elevio_motorDirection(DIRN_STOP);
+        //    StopButton();
+        //}
         elevio_motorDirection(DIRN_STOP);
 
         for(int f=0; f<N_FLOORS; f++){
             for(int b=0; b < N_BUTTONS; b++){
                 for(int i=0; i<orderCount; i++){
                     if(orderList[i].floor>floor){
-                        elevio_motorDirection(DIRN_UP);
+                        while(orderList[i].floor>floor){
+                            floor = elevio_floorSensor();
+                            elevio_motorDirection(DIRN_UP);
+                            StopButton();
                         if (orderList[i].floor==floor){
                             elevio_motorDirection(DIRN_STOP); 
                             void removeOrder(int i); 
+                            printOrders(); 
                         }
+                    }
                          
                         
                     }
@@ -131,9 +137,16 @@ int main(){
         }
 
         if(floor == 0){
-            direction = DIRN_UP;
-        } else if(floor == N_FLOORS-1){
-            direction = DIRN_DOWN;
+            elevio_motorDirection(DIRN_UP);
+        }
+
+        if(floor == N_FLOORS-1){
+            elevio_motorDirection(DIRN_DOWN);
+        }
+
+
+        if(floor >= 0 && floor < 4){
+            elevio_floorIndicator(floor);
         }
 
         for(int f = 0; f < N_FLOORS; f++){
@@ -157,17 +170,6 @@ int main(){
         
         StopButton();
         
-        int nextOrder = findNextOrder(floor, direction);
-        if (nextOrder != -1) {
-            if (nextOrder > floor) {
-                elevio_motorDirection(DIRN_UP);
-            } else if (nextOrder < floor) {
-                elevio_motorDirection(DIRN_DOWN);
-            }
-        } else {
-            elevio_motorDirection(DIRN_STOP);
-        }
-
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
